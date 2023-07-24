01f4b328b4360bb0a739158b70aa3ad43cc98bd8
cccff12 <ksun0430@naver.com>
2023-07-24 16:46:58 +0900

    jsp세팅

--- 화면구현 이후/구현/project_user/src/main/java/com/toes/footprint/models/MemberDto.java
+++ 화면구현 이후/구현/project_user/src/main/java/com/toes/footprint/models/MemberDto.java
@@ -2,10 +2,13 @@ package com.toes.footprint.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
-
+@Setter
+@Getter
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
